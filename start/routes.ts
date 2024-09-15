3b829b8ab08fd7564aaad6c94b275e4bcd96f5d6
Walter Matheus <matheustavares868@gmail.com>
2024-09-14 21:34:43 -0300

    Test implement

--- start/routes.ts
+++ start/routes.ts
@@ -1,36 +1,24 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
 import AutoSwagger from 'adonis-autoswagger'
 import swagger from '#config/swagger'
+
+router.get('/:url', '#controllers/urls_controller.redirectToOriginalUrl')
 
 router.post('url', '#controllers/urls_controller.shortenUrl')
 router.get('url', '#controllers/urls_controller.get').use([middleware.auth()])
 router.put('url/:id', '#controllers/urls_controller.update').use([middleware.auth()])
 router.delete('url/:id', '#controllers/urls_controller.delete').use([middleware.auth()])
 
-router.get('/:url', '#controllers/urls_controller.redirectToOriginalUrl')
-
-/**
- * @requestBody {"code": "xxxxxx"}
- */
 router.post('user', '#controllers/users_controller.post')
 
 router.post('auth', '#controllers/auth_controller.auth')
 
-router.get('/docs/swagger', async () => {
-  return AutoSwagger.default.docs(router.toJSON(), swagger)
-})
-
 // Renders Swagger-UI and passes YAML-output of /swagger
 router.get('/docs/routes', async () => {
   return AutoSwagger.default.ui('/docs/swagger', swagger)
+})
+
+router.get('/docs/swagger', async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger)
 })
--- tests/functional/url/create.spec.ts
+++ tests/functional/url/create.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '@japa/runner'
 import { generateShortUrl } from '../../../app/utils/generateShortUrl.js'
+import User from '../../../app/models/user.js'
 
 test.group('UrlService', () => {
   //  group.beforeEach(() => {
@@ -9,4 +10,84 @@ test.group('UrlService', () => {
   test('should create a new URL code', async ({ assert }) => {
     assert.typeOf(generateShortUrl(), 'string')
   })
+})
+
+test.group('HTTP Testes', () => {
+  test('get a list of users without login', async ({ client }) => {
+    const response = await client.get('/url')
+
+    response.assertStatus(401)
+  })
+
+  test('get a list of users with Login', async ({ client }) => {
+    const user = await User.create({email:"teste@gmail.com", password:123})
+
+    const response = await client.get('/url').withGuard('api_tokens')
+    .loginAs(user)
+
+    response.assertStatus(200)
+  })
+
+  test('Generate short Url', async ({ client }) => {
+    const response = await client.post('/url')
+
+    response.assertStatus(200)
+    response.assertBodyContains({
+      url: "https://localhost:3000/9sad8a"
+    })
+  })
+
+  test('Generate short Url', async ({ client }) => {
+    const response = await client.post('/url')
+
+    response.assertStatus(200)
+    response.assertBodyContains({
+      url: "https://localhost:3000/9sad8a"
+    })
+  })
+
+  test('Edit a Url with auth', async ({ client }) => {
+    const user = await User.create({email:"teste@gmail.com", password:123})
+
+    const response = await client.put('/ur/2').withGuard('api_tokens')
+    .loginAs(user)
+
+    response.assertStatus(200)
+    response.assertBodyContains({
+      id:1,
+      url: "https://localhost:3000/9sad8a"
+    })
+  })
+
+  test('Edit a URL without auth', async ({ client }) => {
+    const response = await client.put('/url/2')
+
+    response.assertStatus(401)
+  })
+
+  test('Delete a Url with auth', async ({ client }) => {
+    const user = await User.create({email:"teste@gmail.com", password:"123#abc"})
+
+    const response = await client.delete('/ur/2').withGuard('api_tokens')
+    .loginAs(user)
+
+    response.assertStatus(200)
+  })
+
+  test('Delete a URL without auth', async ({ client }) => {
+    const response = await client.delete('/url/2')
+
+    response.assertStatus(401)
+  })
+
+  test('Authenticate a user', async ({ client }) => {
+    const response = await client.post('/auth').form({email:"teste@gmail.com", password: "123#abc"})
+
+    response.assertStatus(200)
+    response.assertBodyContains({
+      type:"Bearer",
+      value:"safjfsafhsahfsakdaajfshfakjafsbfaskh"
+    })
+  })
+
 })
